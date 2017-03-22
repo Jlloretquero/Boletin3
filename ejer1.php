3e35de0fc34fb9acaef4b18e722153f0ab5f3310
ajust <ajust@INF1-PC14>
2017-03-22 21:21:32 +0100

    ejer 1

---
+++ ejer1.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "<p>Esta es la primera práctica escrita en PHP</p>";
+        ?>
+        <p>Escrita por Just Berto y Alejandro del alumno</p>
+    </body>
+</html>
