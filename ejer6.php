cb85213d3b314efce02906129b0068dcccccd3ae
jlloret <jlloret@INF2-PC15>
2017-03-24 20:49:48 +0100

    ejer6

---
+++ ejer6.php
@@ -0,0 +1,12 @@
+<?php
+
+$num1=rand(1,50);
+$num2=rand(1,50);
+$num3=rand(1,50);
+$num4=rand(1,50);
+$num5=rand(1,50);
+$estrella1=rand(1,9);
+$estrella2=rand(1,9);
+
+echo $num1." ".$num2." ".$num3." ".$num4." ".$num5." ".$estrella1." ".$estrella2;
+
