20c8b9c596f8e5e899175979420f7139b420366e
jlloret <jlloret@INF2-PC15>
2017-03-24 20:48:22 +0100

    ejer4

---
+++ ejer4.php
@@ -0,0 +1,13 @@
+<?php
+$primera=4;
+$segunda=8;
+$elevado=pow($primera,$segunda);
+$raiz1=sqrt ($primera);
+$raiz2=sqrt ($segunda);
+
+echo $elevado;
+echo "</br>";
+echo $raiz1;
+echo "</br>";
+echo $raiz2;
+?>
